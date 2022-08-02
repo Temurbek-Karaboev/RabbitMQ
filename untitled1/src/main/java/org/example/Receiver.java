c796437a94a904f2db5e15f43e609691f5a08de0
Temurbek-Karaboev <77426587+Temurbek-Karaboev@users.noreply.github.com>
2022-08-02 16:46:00 +0500

    Add files via upload

---
+++ untitled1/src/main/java/org/example/Receiver.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+public class Receiver {
+    private final static String HOST_NAME = "localhost";
+    private final static String QUEUE_NAME = "demo-lab-queue";
+    public static void main(String[] args) throws Exception {
+        final ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost(HOST_NAME);
+        final Connection connection = connectionFactory.newConnection();
+        final Channel channel = connection.createChannel();
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        System.out.println("Waiting for messages from the queue!");
+        final DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            final String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println("Received message from Rabbit MQ  -> " + message);
+        };
+        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> {});
+    }
+}
